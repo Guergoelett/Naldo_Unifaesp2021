3f60ccccf6f44b7318588d2e0a6d893a9c999d30
Naldo Guergolett <naldoguergolett@yahoo.com.br>
2021-10-26 23:38:45 -0300

    Adicionar arquivos de projeto.

---
+++ Naldo_Unifaesp2021/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace Naldo_Unifaesp2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_telefone = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textbox_email = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_estado = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_CEP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_cidade = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_endereco = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox_sobrenome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_nome = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 253);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 100);
+            this.panel1.TabIndex = 44;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(414, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(275, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DELETAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(11, 228);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 19);
+            this.checkBox2.TabIndex = 43;
+            this.checkBox2.Text = "FEMININO";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 185);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(123, 15);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "ORIENTACAO SEXUAL";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 203);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 19);
+            this.checkBox1.TabIndex = 41;
+            this.checkBox1.Text = "MASCULINO";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 15);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "TELEFONE";
+            // 
+            // textBox_telefone
+            // 
+            this.textBox_telefone.Location = new System.Drawing.Point(12, 159);
+            this.textBox_telefone.Name = "textBox_telefone";
+            this.textBox_telefone.Size = new System.Drawing.Size(352, 23);
+            this.textBox_telefone.TabIndex = 39;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(372, 185);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 15);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "E-MAIL";
+            // 
+            // textbox_email
+            // 
+            this.textbox_email.Location = new System.Drawing.Point(371, 203);
+            this.textbox_email.Name = "textbox_email";
+            this.textbox_email.Size = new System.Drawing.Size(351, 23);
+            this.textbox_email.TabIndex = 37;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 15);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "ESTADO";
+            // 
+            // textBox_estado
+            // 
+            this.textBox_estado.Location = new System.Drawing.Point(11, 115);
+            this.textBox_estado.Name = "textBox_estado";
+            this.textBox_estado.Size = new System.Drawing.Size(352, 23);
+            this.textBox_estado.TabIndex = 35;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(370, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 15);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "CEP";
+            // 
+            // textBox_CEP
+            // 
+            this.textBox_CEP.Location = new System.Drawing.Point(370, 159);
+            this.textBox_CEP.Name = "textBox_CEP";
+            this.textBox_CEP.Size = new System.Drawing.Size(352, 23);
+            this.textBox_CEP.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "CIDADE";
+            // 
+            // textBox_cidade
+            // 
+            this.textBox_cidade.Location = new System.Drawing.Point(370, 71);
+            this.textBox_cidade.Name = "textBox_cidade";
+            this.textBox_cidade.Size = new System.Drawing.Size(352, 23);
+            this.textBox_cidade.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "ENDEREÇO";
+            // 
+            // textBox_endereco
+            // 
+            this.textBox_endereco.Location = new System.Drawing.Point(11, 71);
+            this.textBox_endereco.Name = "textBox_endereco";
+            this.textBox_endereco.Size = new System.Drawing.Size(352, 23);
+            this.textBox_endereco.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "DATA DE NASCIMENTO";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(370, 115);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(352, 23);
+            this.dateTimePicker1.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "SOBRENOME";
+            // 
+            // textbox_sobrenome
+            // 
+            this.textbox_sobrenome.Location = new System.Drawing.Point(370, 27);
+            this.textbox_sobrenome.Name = "textbox_sobrenome";
+            this.textbox_sobrenome.Size = new System.Drawing.Size(352, 23);
+            this.textbox_sobrenome.TabIndex = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "NOME";
+            // 
+            // textBox_nome
+            // 
+            this.textBox_nome.Location = new System.Drawing.Point(12, 27);
+            this.textBox_nome.Name = "textBox_nome";
+            this.textBox_nome.Size = new System.Drawing.Size(352, 23);
+            this.textBox_nome.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 359);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(711, 339);
+            this.textBox1.TabIndex = 45;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 710);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_telefone);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textbox_email);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_estado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_CEP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_cidade);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_endereco);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textbox_sobrenome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_telefone;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textbox_email;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_estado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_CEP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_cidade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_endereco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textbox_sobrenome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_nome;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Naldo_Unifaesp2021/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naldo_Unifaesp2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newLine = Environment.NewLine;
+            String Sexo = null;
+            if (checkBox1.Checked == true)
+            { Sexo = "MASCULINO"; }
+            else if (checkBox2.Checked == true)
+            { Sexo = "FEMININO"; }
+
+            textBox1.Text = "Nome: " + textBox_nome.Text.ToString() + newLine +
+                "Sobrenome: " + textbox_sobrenome.Text.ToString() + newLine +
+                "Data de nascimento: " + dateTimePicker1.Text.ToString() + newLine +
+                "Sexo: " + Sexo +
+                "Endereço: " + textBox_endereco.Text.ToString() + newLine +
+                "Cidade: " + textBox_cidade.Text.ToString() + newLine +
+                "Estado: " + textBox_estado.Text.ToString() + newLine +
+                "CEP: " + textBox_CEP.Text.ToString() + newLine +
+                "Telefone para contato: " + textBox_telefone.Text.ToString() + newLine +
+                "E-mail: " + textbox_email.Text.ToString();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+        }
+    }
+}
